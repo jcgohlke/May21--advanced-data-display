4afd5421b1c9bcb563bb91230a557a0ace3a3612
Ben Gohlke <jcgohlke@me.com>
2021-05-04 21:09:52 -0400

    Finished guided project for 3.1

--- lesson-3.1/gp-starter/BasicCollectionView/BasicCollectionViewController.swift
+++ lesson-3.1/gp-starter/BasicCollectionView/BasicCollectionViewController.swift
@@ -45,25 +45,68 @@ class BasicCollectionViewController: UICollectionViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    collectionView.setCollectionViewLayout(generateLayout(), animated: false)
+  }
+  
+  // MARK: Collection View Layout
+  
+  private func generateLayout() -> UICollectionViewLayout {
+    let spacing: CGFloat = 10
+    
+    // Item definition
+    let itemSize = NSCollectionLayoutSize(
+      widthDimension: .fractionalWidth(1.0),
+      heightDimension: .fractionalHeight(1.0)
+    )
+    let item = NSCollectionLayoutItem(layoutSize: itemSize)
+    
+    // Group definition
+    let groupSize = NSCollectionLayoutSize(
+      widthDimension: .fractionalWidth(1.0),
+      heightDimension: .absolute(70.0)
+    )
+    let group = NSCollectionLayoutGroup.horizontal(
+      layoutSize: groupSize,
+      subitem: item,
+      count: 1)
+    group.contentInsets = NSDirectionalEdgeInsets(
+      top: spacing,
+      leading: 0,
+      bottom: 0,
+      trailing: 0
+    )
+    
+    let section = NSCollectionLayoutSection(group: group)
+    section.contentInsets = NSDirectionalEdgeInsets(
+      top: spacing,
+      leading: spacing,
+      bottom: spacing,
+      trailing: spacing
+    )
+    
+    let layout = UICollectionViewCompositionalLayout(section: section)
+    
+    return layout
   }
   
   // MARK: UICollectionView Data Source
   
   override func numberOfSections(in collectionView: UICollectionView) -> Int {
     // #warning Incomplete implementation, return the number of sections
-    return 1
+    return sectionedStates.count
   }
-  
   
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     // #warning Incomplete implementation, return the number of items
-    return 1
+    return sectionedStates[section].count
   }
   
   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! BasicCollectionViewCell
     
     // Configure the cell
+    cell.label.text = sectionedStates[indexPath.section][indexPath.item]
     
     return cell
   }
